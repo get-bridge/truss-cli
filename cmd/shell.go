9d732d0435da9b175d52ed4755f9748e5497f2e7
JD Harrington <jdharrington@instructure.com>
2020-07-08 17:00:21 -0400

    Cleanup shell command

--- cmd/shell.go
+++ cmd/shell.go
@@ -10,23 +10,12 @@ import (
 var shellCmd = &cobra.Command{
 	Use:   "shell",
 	Short: "Launch a shell container in a Truss cluster",
-	Long:  "TBD...",
+	Long:  "Launches a shell Pod in a Truss cluster. Useful for debugging purposes.",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("shell called")
 	},
 }
 
 func init() {
-	shellCmd.AddCommand(shellNodeCmd)
 	rootCmd.AddCommand(shellCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// shellCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// shellCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
