2a59de9d9421b3161b16a11b3a3845ffc9494b1a
JD Harrington <jd@jdharrington.net>
2022-11-10 12:53:21 -0500

    Duplicate bootstramp template

---
+++ bootstrap-templates/default-2022/staging/cmh/config.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket   = "bridge-shared-terraform-us-east-2"
+    key      = "{{ .Params.name }}/{{ .TrussDir }}/staging/cmh/terraform.tfstate"
+    region   = "us-east-2"
+    acl      = "bucket-owner-full-control"
+    encrypt  = true
+    role_arn = "arn:aws:iam::127178877223:role/xacct/ops-admin"
+  }
+}
+
+provider "aws" {
+  region              = "us-east-2"
+  allowed_account_ids = ["127178877223"]
+
+  assume_role {
+    role_arn = "arn:aws:iam::127178877223:role/xacct/ops-admin"
+  }
+}
